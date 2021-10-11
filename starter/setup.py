315a37e79312566af4cb4aae07c36fd50cd5ccf6
nicholasdinicola@outlook.com <nicholasdinicola@outlook.com>
2021-10-11 10:00:53 +0200

    requirements and setup.py updated

--- starter/setup.py
+++ starter/setup.py
@@ -1,8 +1,10 @@
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+setup(
     name="starter",
     version="0.0.0",
     description="Starter code.",
     author="Student",
+    packages=["starter"],
+    install_requires=['wheel', 'bar', 'greek'],  # external packages as dependencies
 )
